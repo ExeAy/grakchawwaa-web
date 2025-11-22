6dafa94b65ad2e3028cce612678a2f1b5254feb1
ExeAy <hannes@falkanra.eu>
2025-11-22 18:20:39 +0100

    Initial page

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Under construction</h1>
+      <p className="text-lg">
+        We are working on the site and it will be available soon.
+      </p>
+      <p className="text-lg">
+        Thank you for your patience.
+      </p>
+    </div>
+  );
+}
